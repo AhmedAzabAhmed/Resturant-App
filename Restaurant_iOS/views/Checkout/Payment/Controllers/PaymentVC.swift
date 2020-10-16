bcbb507c29bbbd6e0eda19baa8ec1e67959459e9
azab <azab>
2020-10-16 19:52:24 +0200

    update

--- Restaurant_iOS/views/Checkout/Payment/Controllers/PaymentVC.swift
+++ Restaurant_iOS/views/Checkout/Payment/Controllers/PaymentVC.swift
@@ -10,17 +10,52 @@ import UIKit
 
 class PaymentVC: UIViewController {
 
+    let datePicker = UIDatePicker()
+    
     @IBOutlet weak var paymentView: UIView!
     
-    
+    @IBOutlet weak var epireDateField: UITextField!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        showDatePicker()
+
+        epireDateField.inputView = datePicker
         
         paymentView.layer.cornerRadius = 10
     }
+       
     
+    func showDatePicker(){
+           //Formate Date
+           datePicker.datePickerMode = .date
+           
+           //ToolBar
+           let toolbar = UIToolbar();
+           toolbar.sizeToFit()
+           let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(donedatePicker));
+           let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+           let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelDatePicker));
+           
+           toolbar.setItems([doneButton,spaceButton,cancelButton], animated: false)
+           
+           epireDateField.inputAccessoryView = toolbar
+           epireDateField.inputView = datePicker
+           
+       }
+    
+    @objc func donedatePicker(){
+           
+           let formatter = DateFormatter()
+           formatter.dateFormat = "dd/MM/yyyy"
+           epireDateField.text = formatter.string(from: datePicker.date)
+           self.view.endEditing(true)
+       }
+    @objc func cancelDatePicker(){
+        self.view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
